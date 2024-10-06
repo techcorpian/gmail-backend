40d985b5efdd375bbe4e483a94990ef57a0987aa
mushthaq <techcorpian@gmail.com>
2024-10-06 22:17:42 +0530

    addition of nodemailer

--- controllers/InboxController.js
+++ controllers/InboxController.js
@@ -1,5 +1,7 @@
 import Message from '../models/InboxModel.js';
 import Sent from '../models/SentModel.js';
+// const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 export class InboxController {
   static async getInbox(req, res) {
@@ -16,13 +18,31 @@ export class InboxController {
   static async insertInbox(req, res) {
     const { addressArr, subject, message, userFirstname, userLastname, userMail } = req.body;
     const title = `${userFirstname} ${userLastname}`;
+    
+    // Create Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      service: 'gmail', // You can choose any email service like Outlook, Yahoo, etc.
+      auth: {
+        user: 'mushthaq.na@gmail.com', // Replace with your email
+        pass: 'arrp vttx vrut ttjq',  // Replace with your email password or app-specific password
+      },
+    });
   
     try {
       const promises = addressArr.map(async (element) => {
+        // Save messages in the database
         const newMessage = new Message({ toAddress: element, subject, message, title, fromAddress: userMail });
         const newSent = new Sent({ toAddress: element, subject, message, title, fromAddress: userMail });
         await newMessage.save();
         await newSent.save();
+  
+        // Send email using Nodemailer
+        await transporter.sendMail({
+          from: userMail,
+          to: element,
+          subject,
+          text: message, // You can also use 'html' to send HTML formatted email
+        });
       });
   
       await Promise.all(promises);
@@ -32,6 +52,7 @@ export class InboxController {
       res.status(500).json({ err });
     }
   }
+  
 
   static async viewInbox(req, res) {
     try {
